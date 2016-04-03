570b126dc43557b999dbf142b3b9e2b346bd9332
Alexey T <workspace@com.ua>
2016-04-03 19:04:47 +0300

    repost fix and image

--- lab0.R
+++ lab0.R
@@ -1,28 +1,37 @@
-source("arbuthnot.R")
-dim(arbuthnot)
-str(arbuthnot)
-names(arbuthnot)
-arbuthnot$boys
-arbuthnot$girls
-plot(x = arbuthnot$year, y = arbuthnot$boys)
-plot(x = arbuthnot$year, y = arbuthnot$girls, type = "l")
-plot(x = arbuthnot$year, y = arbuthnot$boys + arbuthnot$girls, type = "l")
-plot(x = arbuthnot$year, y = arbuthnot$boys/arbuthnot$girls, type = "l")
-plot(x = arbuthnot$year, y = arbuthnot$boys/(arbuthnot$boys + arbuthnot$girls), type = "l")
-arbuthnot$boys > arbuthnot$girls
+# file 'present.R' and 'arbuthnot.R' downloaded in lab0 folder
+source("lab0/arbuthnot.R")
+source("lab0/present.R")
 
 #1
-source("present.R")
+summary(present$year)
 dim(present)
 names(present)
-#2
-str(present)
-unique(present$year)
-#3
-plot(x = present$year, y=present$boys/present$girls, type = "l")
 
-present$boys > present$girls
+#2
+summary(present) #2.1
+summary(arbuthnot)
+
+colMeans(present)[1]-colMeans(arbuthnot)[1] #2.2
+colMeans(present)/colMeans(arbuthnot)
+
+#3
+B2G_RationPresent <-present$boys/present$girls
+B2G_RationArbuthnot <-arbuthnot$boys/arbuthnot$girls
+
+png(filename = "lab0/boy-to-girl_ratio.png", width = 960, height = 480,
+    pointsize = 12, bg = "white")
+
+plot(x = present$year, y=B2G_RationPresent, type = "l",
+     xlab="Year", ylab="Boy-to-girl Ratio")
+
+
+dev.off()
+plot(x = arbuthnot$year, y=B2G_RationArbuthnot, type = "l",
+     xlab="Year", ylab="Boy-to-girl Ratio")
+#present$boys > present$girls
 #4.
+
+present[which.max(present$boys + present$girls),1] 
+
 plot(x = present$year, y=present$boys+present$girls, type = "l")
 
-#ggplot2, leaflet, shiny. 
