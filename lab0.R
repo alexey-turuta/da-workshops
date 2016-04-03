8d7f01957eef30a4ead28780132754c99b343a36
Alexey T <workspace@com.ua>
2016-04-03 21:20:24 +0300

    lab0

--- lab0.R
+++ lab0.R
@@ -15,23 +15,28 @@ colMeans(present)[1]-colMeans(arbuthnot)[1] #2.2
 colMeans(present)/colMeans(arbuthnot)
 
 #3
-B2G_RationPresent <-present$boys/present$girls
-B2G_RationArbuthnot <-arbuthnot$boys/arbuthnot$girls
+summary(present$boys/present$girls)
 
 png(filename = "lab0/boy-to-girl_ratio.png", width = 960, height = 480,
     pointsize = 12, bg = "white")
-
-plot(x = present$year, y=B2G_RationPresent, type = "l",
+plot(boys/girls ~ year, data = present, 
+     main = "Figure 1. Boy to girl ratio",
+     type = "l",  ylim = c(1.045, 1.06),
      xlab="Year", ylab="Boy-to-girl Ratio")
-
-
 dev.off()
 plot(x = arbuthnot$year, y=B2G_RationArbuthnot, type = "l",
      xlab="Year", ylab="Boy-to-girl Ratio")
-#present$boys > present$girls
+
+summary(present$boys/(present$girls + present$boys))
+
+png(filename = "lab0/boy-to-all_proportion.png", width = 960, height = 480,
+    pointsize = 12, bg = "white")
+plot(boys/(girls+boys) ~ year, data = present, 
+     main = "Figure 2. Boys proportion",
+     type = "l",  ylim = c(0.51, 0.515),
+     xlab="Year", ylab="Boys proportion")
+dev.off()
 #4.
 
 present[which.max(present$boys + present$girls),1] 
-
-plot(x = present$year, y=present$boys+present$girls, type = "l")
 
